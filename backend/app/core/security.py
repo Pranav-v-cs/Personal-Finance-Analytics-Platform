5b117ee591f5bfec37d854695cd88a842d563dfa
Pranav <23z139@psgitech.ac.in>
2026-06-03 00:44:01 +0530

    Complete JWT authentication flow

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,4 +1,13 @@
 from passlib.context import CryptContext # type: ignore
+from datetime import datetime, timedelta, timezone
+from jose import jwt, JWTError # type: ignore
+
+from app.core.config import settings
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="auth/login"
+)
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
@@ -18,3 +27,33 @@ def verify_password(
         plain_password,
         hashed_password
     )
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
+
+    return encoded_jwt
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM]
+        )
+
+        return payload
+
+    except JWTError:
+        return None
